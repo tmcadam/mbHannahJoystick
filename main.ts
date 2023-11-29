f5aae8152dc466e399f8b59c81bc2817cf46982c
Tom McAdam <tmcadam@users.noreply.github.com>
2023-11-29 14:59:55 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+joystickbit.initJoystickBit()
+radio.setGroup(1)
+joystickbit.Vibration_Motor(100)
+basic.showIcon(IconNames.StickFigure)
+basic.forever(function () {
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 200) {
+        radio.sendNumber(4)
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.X) > 800) {
+        radio.sendNumber(3)
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) < 200) {
+        radio.sendNumber(2)
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) > 800) {
+        radio.sendNumber(1)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
+        radio.sendNumber(5)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
+        radio.sendNumber(6)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P14)) {
+        radio.sendNumber(7)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P15)) {
+        radio.sendNumber(8)
+    }
+})
